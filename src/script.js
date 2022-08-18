5545b17a371f71cea4ad62f593e39f1602b84858
AnkitSainiYNR <ankit1825.be20@chitkara.edu.in>
2022-08-18 13:38:52 +0530

    some changes

--- src/script.js
+++ src/script.js
@@ -1,4 +1,3 @@
-// add class navbarDark on navbar scroll
 const header = document.querySelector(".navbar");
 console.log(header);
 window.onscroll = function () {
@@ -9,12 +8,6 @@ window.onscroll = function () {
     header.classList.add("navbarDark");
   }
 };
-// collapse navbar after click on small devices
+
 const navLinks = document.querySelectorAll(".nav-item");
 const menuToggle = document.getElementById("navbarSupportedContent");
-
-// navLinks.forEach((l) => {
-//   l.addEventListener("click", () => {
-//     new bootstrap.Collapse(menuToggle).toggle();
-//   });
-// });
